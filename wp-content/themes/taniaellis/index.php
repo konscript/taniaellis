8a6a19e3bf8a84dd9f1b9700307fd827dc535085
Kristian Andersen <ksm.andersen@gmail.com>
2011-06-06 14:03:49 +0200

    Added basic html structure to footer and header. Imported external stylesheets. Made all fonts Arial.

---
+++ wp-content/themes/taniaellis/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Theme Name: Tania Ellis
+Theme URI: http://taniaellis.com/
+Description: Default theme for the Tania Ellis website.
+Author: Konscript
+Author URI: http://konscript.com/
+Version: 0.1
+Tags: konscript, tania, ellis
+*/
+
+get_header(); ?>
+
+<h1>It's a trap!</h1>
+
+<? get_footer(); ?>
