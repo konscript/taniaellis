cf4e01be2fe0ca1d5ca9703740a6b455ae6e881f
Kristian Andersen <ksm.andersen@gmail.com>
2011-06-06 14:55:38 +0200

    Added navigation menu. Added sliced images from psd

---
+++ wp-content/themes/taniaellis/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+if(function_exists( 'register_nav_menus')) {
+	register_nav_menus(array("navigation-top" => "Top Menu"));
+}
+
+function get_navigation_for_menu_location($location) {
+	$locations = get_nav_menu_locations();
+	$menu = wp_get_nav_menu_object($locations[$location]);
+	$term_id = $menu->term_id;
+	
+	$s = "";
+	foreach((array) wp_get_nav_menu_items($term_id) as $key => $item) {
+		$s .= '<a href="'.$item->url.'">'.$item->title.'</a>';
+	}
+	
+	return $s;
+}
+
+?>
