bab880d6d9cd8916f9a115d54e690df9ac5fb7d2
Tomas Lieberkind <t.lieberkind@gmail.com>
2011-06-07 20:28:59 +0200

    Added Tania figure to frontpage header

--- wp-content/themes/taniaellis/home.php
+++ wp-content/themes/taniaellis/home.php
@@ -24,7 +24,8 @@
     ?>
     
     <div id="frontpage-header-content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <img src="<?php bloginfo('template_url'); ?>/images/header_frontpage_tania_telly.png" />
+        <iframe src="http://player.vimeo.com/video/23945339?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0" width="276" height="177" frameborder="0"></iframe>
     </div>
     
 </div>
