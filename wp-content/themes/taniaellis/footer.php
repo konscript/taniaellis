17deaac2f958cdcb8dd8b12895dc5b74b33d7408
Kristian Andersen <ksm.andersen@gmail.com>
2011-06-09 17:30:14 +0200

    Fixed footer bug in ie and ff

---
+++ wp-content/themes/taniaellis/footer.php
@@ -0,0 +1,14 @@
+	</div> <!-- #page-wrapper -->
+
+	<footer>
+		<ul id="footer-bar">
+	    	<li>Address: Larsbjornsstraede 13, 1454 Copenhagen</li>
+	    	<li>T: +45 3214 2295</li>
+	    	<li>M: +45 2625 2295</li>
+	    	<li>E-mail: te[at]taniaellis[dot]dk</li>
+	    	<li>www.taniaellis.dk</li>
+		</ul>
+		<a href="http://www.ingenco2.dk/" target="_blank" id="co2-link"><img src="<?php bloginfo('template_url') ?>/images/co2_neutral_website.png" alt="CO2 neutral hjemmeside" /></a>
+	</footer>
+	</body>
+</html>
--- wp-content/themes/taniaellis/home.php
+++ wp-content/themes/taniaellis/home.php
@@ -1,52 +1,5 @@
-<!DOCTYPE>
-<html>
-	<head>
-		<title>Tania Ellis</title>
-		<meta charset="utf-8" />
-		
-		<meta name="description" content="Social megatrends :: CSR and CSI :: social responsibility and social innovation :: ethics and meaning :: sustainable business :: leadership" />
-		<meta name="keywords" content="" />
-		<meta name="author" content="Tania Ellis" />
-		
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url') ?>" />
-		
-		<script src="<?php bloginfo('template_url') ?>/js/eCSStender.js"></script>
-		<!--[if lt IE 9]>
-		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-	</head>
-	
-	<body>
-	    <div id="page-wrapper">
-    		<header>
-    			<div id="header-top-container">
-					<form method="get" action="<?php bloginfo('siteurl') ?>/search/">
-						<input type="submit" value="" />    			    	
-						<input type="text" name="searchbar" value="" id="searchbar" />
-						
-					</form>
-    				<?php wp_nav_menu(array(
-    					'theme_location'		=> 'navigation-top',
-    					'container'				=> 'nav',
-    					'menu_id'				=> 'navigation-top',
-    					'menu_class'			=> ''
-    				)); ?>
-    			</div>
-    			
-    			<div id="header-menu-logo-container">
-    			    
-                    <?php wp_nav_menu(array(
-                        'theme_location'        => 'navigation-pages',
-                        'container'             => 'nav',
-                        'menu_id'               => 'navigation-pages',
-                        'menu_class'            => ''
-                    )); ?>
-    				<img id="logo" src="<?php bloginfo('template_url')?>/images/logo.png" />
-    				<a id="shop-link" href="javascript:void(0)">Shop</a>
-    			</div>
-
-    		</header>
-		
+<?php get_header(); ?>
+			
     		<div id="header-content">
 				<p id="sub-heading">The <span>Social</span> Business Company</p>
 				<p id="language-picker">
@@ -121,17 +74,4 @@
 				
 			</div> <!-- #page-content -->
 			
-		</div> <!-- #page-wrapper -->
-		
-    	<footer>
-  			<ul id="footer-bar">
-  			    <li>Address: Larsbjornsstraede 13, 1454 Copenhagen</li>
-  			    <li>T: +45 3214 2295</li>
-  			    <li>M: +45 2625 2295</li>
-  			    <li>E-mail: te[at]taniaellis[dot]dk</li>
-  			    <li>www.taniaellis.dk</li>
-  			</ul>
-  			<a href="http://www.ingenco2.dk/" target="_blank" id="co2-link"><img src="<?php bloginfo('template_url') ?>/images/co2_neutral_website.png" alt="CO2 neutral hjemmeside" /></a>
-      	</footer>
-	</body>
-</html>
+<?php get_footer(); ?>
