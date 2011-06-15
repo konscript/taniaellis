d7864f6eff91579225fa8b89406bc128f5d7519b
Tomas Lieberkind <t.lieberkind@gmail.com>
2011-06-15 12:55:19 +0200

    Added club.php

---
+++ wp-content/themes/taniaellis/club.php
@@ -0,0 +1,76 @@
+<?php
+/*
+    Template Name: Club
+*/
+?>
+<?php get_header(); ?>
+			
+    		<div id="header-content">
+				<p id="sub-heading">The <span>Social</span> Business Company</p>
+				<p id="language-picker">
+		    		<a class="current" href="javascript:void(0)">ENG</a> / <a href="javascript:void(0)">DK</a>
+				</p>
+				
+				<div class="clearer"></div>
+
+				<div id="frontpage-header-container">    
+
+    
+	    			<div id="frontpage-header-content">
+                        <!-- <img id="intro-movie" src="<?php bloginfo('template_url'); ?>/images/header_frontpage_tania_telly.png" /> -->
+	        			<!-- Video goes here! -->
+	
+
+                        <!-- <div class="header-quote">
+                            <h6>Heartcore business for social and economic value!</h6>
+                            <p id="header-quote-text">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    
+                            <div id="read-more-signature">
+                                <a href="javascript:void(0)">Read more</a>
+                            </div>
+                        </div> -->
+	
+	        			<div id="newsletter-signup-box">
+	            			<p id="header">
+	                			<span id="social-business-trends">Social business trends</span>
+	                			<span id="newsletter">Newsletter</span>
+	            			</p>
+	            			
+							<p id="content">Sign up for my newsletter and get the FREE bonus guide along with monthly inspiration on social business and heartcore value. Sign up and I will email you the guide.</p>
+	            			<a id="newsletter-signup-button" href="javascript:void(0)">Subscribe</a>
+	        			</div>
+	
+						<ul id="social-bar">
+							<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" alt="YouTube" title="YouTube" /></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" alt="RSS" title="RSS" /></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_twitter.png" alt="Twitter" title="Twitter" /></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_facebook.png" alt="Facebook" title="Facebook" /></a></li>
+							<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" alt="Fairpages" title="Fairpages" /></a></li>
+						</ul>
+						
+						<img src="<?php bloginfo('template_url') ?>/images/newsletter_bonus_sticker.png" alt="" id="bouns-sticker" />
+	    			</div> <!-- #frontpage-header-content -->
+				</div> <!-- #frontpage-header-container -->
+				
+				
+			</div> <!-- #header-content -->
+			
+			<div class="clearer"></div>
+
+			<div id="page-content">
+				<div id="page">
+					<section class="left-sidebar">
+		    			&nbsp;
+					</section>
+					
+					<section class="right-sidebar">
+					    &nbsp;
+					</section>
+					
+					<div class="clearer"></div>
+					
+				</div> <!-- #page -->
+				
+			</div> <!-- #page-content -->
+
+<?php get_footer(); ?>
