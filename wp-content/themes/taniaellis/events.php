379f73ac15b2a8030f6e419e19d7bff8be163657
Kristian Andersen <ksm.andersen@gmail.com>
2011-08-13 17:00:18 +0200

    Added events menu to event page

--- wp-content/themes/taniaellis/events.php
+++ wp-content/themes/taniaellis/events.php
@@ -16,7 +16,7 @@
 	<div id="header-container">
 		
 		<?php wp_nav_menu(array(
-	    'theme_location' 	=> 'reading-room-menu',
+	    'theme_location' 	=> 'events-menu',
 			'menu_class'		=> 'navigation-header',
 			'menu_id'			=> 'navigation-header-standard',
 			'link_before'		=> '<span>&nbsp;</span>'
