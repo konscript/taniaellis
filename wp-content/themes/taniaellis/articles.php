287802b6b8decab5ac1e0e6166b03a8442b18bbd
Tomas Lieberkind <t.lieberkind@gmail.com>
2011-09-04 16:41:36 +0200

    Added primary category to articles.

--- wp-content/themes/taniaellis/articles.php
+++ wp-content/themes/taniaellis/articles.php
@@ -7,7 +7,7 @@ Template Name: Articles
 <?php get_header(); ?>
 
 <div id="header">
-  <p id="sub-heading">The <span>Social</span> Business Company</p>
+  <p id="sub-heading">The <span>Social</span> Business Company &reg;</p>
   <p id="language-picker">
     <a class="current" href="javascript:void(0)">ENG</a> / <a href="javascript:void(0)">DK</a>
   </p>
