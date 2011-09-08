db570b3057c816a1bbe1b3d3146a0d8a2b700ce0
Kristian Andersen <ksm.andersen@gmail.com>
2011-09-08 17:02:43 +0200

    Fixed sidebar on article categories

--- wp-content/plugins/te_post-stream-widget/templates/post.php
+++ wp-content/plugins/te_post-stream-widget/templates/post.php
@@ -15,6 +15,12 @@
 		<a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		<p class="excerpt"><?php the_excerpt_rss(); ?></p>
 		
+		<?php if(function_exists('wp_gdsr_render_article') && $instance['showRatings']) : ?>
+			<div class="rating">
+				<?php wp_gdsr_render_article(); ?>
+			</div>
+		<?php endif; ?>
+		
 		<div class="options">
 			<a href="<?php the_permalink(); ?>" class="read-more">Read more</a>
 		</div>
