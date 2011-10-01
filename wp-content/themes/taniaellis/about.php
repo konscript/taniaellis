8bad53329bab8b4e5d9ffdf9682a273f726f83eb
Kristian Andersen <ksm.andersen@gmail.com>
2011-10-01 13:49:38 +0200

    Fixed social media icons

--- wp-content/themes/taniaellis/about.php
+++ wp-content/themes/taniaellis/about.php
@@ -53,13 +53,14 @@
 <div id="page-content">
 	<div id="page">
 	  
-	  <div id="social-bar" class="addthis_toolbox addthis_32x32_style">
-      <ul>
-		  	<li><a class="addthis_button_facebook"></a></li>
-			  <li><a class="addthis_button_twitter"></a></li>
-			  <li><a class="addthis_button_linkedin"></a></li>
-			  <li><a class="addthis_button_youtube"></a></li>
-			  <li><a class="addthis_button_rss"></a></li>
+	  <div id="social-bar">
+		  <ul>
+		  	<li><a target="_blank" href="http://www.facebook.com/pages/TANIA-­‐ELLIS/110480409004141"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_facebook.png" /></a></li>
+			  <li><a target="_blank" href="http://twitter.com/TaniaEllis_DK"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_twitter.png" /></a></li>
+			  <li><a target="_blank" href="http://www.linkedin.com/groups?mostPopular=&gid=3776885"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_linkedin.png" /></a></li>
+			  <li><a target="_blank" href="http://www.youtube.com/TheNewPioneers"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" /></a></li>
+			  <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" /></a></li>
+			  <!-- <li><a target="_blank" href="http://thefairpages.com/company-­‐profile/profile.aspx?company=tania-­‐ellis"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" /></a></li>//-->
 			</ul>
 			<div class="box">
         <p class="box-header">
--- wp-content/themes/taniaellis/readingroom.php
+++ wp-content/themes/taniaellis/readingroom.php
@@ -62,12 +62,13 @@
 	<div id="page">
 	  
 	  <div id="social-bar">
-      <ul>
-				<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" alt="YouTube" title="YouTube" /></a></li>
-				<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" alt="RSS" title="RSS" /></a></li>
-				<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_twitter.png" alt="Twitter" title="Twitter" /></a></li>
-				<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_facebook.png" alt="Facebook" title="Facebook" /></a></li>
-				<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" alt="Fairpages" title="Fairpages" /></a></li>
+		  <ul>
+		  	<li><a target="_blank" href="http://www.facebook.com/pages/TANIA-­‐ELLIS/110480409004141"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_facebook.png" /></a></li>
+			  <li><a target="_blank" href="http://twitter.com/TaniaEllis_DK"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_twitter.png" /></a></li>
+			  <li><a target="_blank" href="http://www.linkedin.com/groups?mostPopular=&gid=3776885"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_linkedin.png" /></a></li>
+			  <li><a target="_blank" href="http://www.youtube.com/TheNewPioneers"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" /></a></li>
+			  <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" /></a></li>
+			  <!-- <li><a target="_blank" href="http://thefairpages.com/company-­‐profile/profile.aspx?company=tania-­‐ellis"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" /></a></li>//-->
 			</ul>
 			<div class="box">
         <p class="box-header">
