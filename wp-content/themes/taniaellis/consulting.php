bd3353370122e1bbde7c4a71745fc847ffcedce9
Kristian Andersen <ksm.andersen@gmail.com>
2011-10-01 14:12:53 +0200

    Added fairpages icon. Reduces icon size to 27x27

--- wp-content/themes/taniaellis/consulting.php
+++ wp-content/themes/taniaellis/consulting.php
@@ -92,7 +92,7 @@
 			  <li><a target="_blank" href="http://www.linkedin.com/groups?mostPopular=&gid=3776885"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_linkedin.png" /></a></li>
 			  <li><a target="_blank" href="http://www.youtube.com/TheNewPioneers"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" /></a></li>
 			  <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" /></a></li>
-			  <!-- <li><a target="_blank" href="http://thefairpages.com/company-­‐profile/profile.aspx?company=tania-­‐ellis"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" /></a></li>//-->
+			  <li><a target="_blank" href="http://thefairpages.com/company-profile/profile.aspx?company=tania-ellis"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" /></a></li>
 			</ul>
 			<div class="box">
         <p class="box-header blue-header">
