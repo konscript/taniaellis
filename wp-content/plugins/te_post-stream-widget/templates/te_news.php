992a124be81ac93b39a47c5d375f5100f0c7fb9a
Kristian Andersen <ksm.andersen@gmail.com>
2011-10-25 14:38:28 +0200

    Added single page for news

---
+++ wp-content/plugins/te_post-stream-widget/templates/te_news.php
@@ -0,0 +1,35 @@
+<div class="item news">
+	<div class="item-content">
+		<?php if(has_post_thumbnail() && $instance['thumbnails']) : ?>
+      <div class="thumb-wrapper">
+        <div class="thumb-container">
+					<a href="<?php the_permalink(); ?>">
+          	<?php the_post_thumbnail('post-square-thumbnail', array('class' => 'featured-image')); ?>
+					</a>
+        </div>
+      </div>
+    <?php endif; ?>
+	
+		<p class="meta-data"><?php the_time('j M Y'); ?></p>
+		
+		<span class="by-line">
+			<?php
+        $byline = get_post_meta(get_the_ID(), 'te_news-options-id', true);
+        
+        if($byline)
+          echo $byline;
+        else
+          echo "By " . get_the_author();
+      ?>
+		</span>
+		
+		<a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<p class="excerpt"><?php the_excerpt_rss(); ?></p>
+		
+		<div class="options">
+			<a href="<?php the_permalink(); ?>" class="read-more">Read more</a>
+		</div>
+		
+		<div class="clearer"></div>
+	</div>
+</div>
