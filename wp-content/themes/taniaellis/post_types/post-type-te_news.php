c5a4d1fd73585f8d3084ba15674d409b22bd5d99
Tomas Lieberkind <t.lieberkind@gmail.com>
2011-10-25 13:44:43 +0200

    Added missing file

---
+++ wp-content/themes/taniaellis/post_types/post-type-te_news.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+#########################
+# CREATE NEWS POST TYPE #
+#########################
+**/
+add_action('init', 'news_register');
+
+function news_register() {
+
+  $labels = array(
+		'name'                => _x('News', 'post type general name'),
+		'singular_name'       => _x('News', 'post type singular name'),
+		'add_new'             => _x('Add', 'news'),
+		'add_new_item'        => __('Add News'),
+		'edit_item'           => __('Edit News'),
+		'new_item'            => __('New News'),
+		'view_item'           => __('View News'),
+		'search_items'        => __('Search News'),
+		'not_found'           => __('Nothing found'),
+		'not_found_in_trash'  => __('Nothing found in Trash'),
+		'parent_item_colon'   => ''
+	);
+	
+	$args = array(
+	  '_builtin'            => false,
+		'labels'              => $labels,
+		'public'              => true,
+		'publicly_queryable'  => true,
+		'show_ui'             => true,
+		'query_var'           => true,
+		'menu_position'       => 5,
+		'_builtin'            => false, // It's a custom post type, not built in!
+	  'menu_icon'           => get_stylesheet_directory_uri() . '/images/icon_news.png',
+		'rewrite'             => array('slug' => 'about/company-news', 'with_front' => false),
+		'capability_type'     => 'post',
+		'hierarchical'        => false,
+		'supports'            => array('title', 'editor', 'thumbnail')
+	  );
+	  
+	  register_post_type('te_news', $args);
+}
+
+?>
