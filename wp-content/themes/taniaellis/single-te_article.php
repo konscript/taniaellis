96be0f695c4687567f572e230a934444421ea490
Tomas Lieberkind <t.lieberkind@gmail.com>
2011-11-24 12:06:57 +0100

    Corrected image size on articles

--- wp-content/themes/taniaellis/single-te_article.php
+++ wp-content/themes/taniaellis/single-te_article.php
@@ -63,7 +63,7 @@
                                     <?php if(has_post_thumbnail($post->ID)) : ?>
                                       <div class="thumb-wrapper">
                                         <div class="thumb-container">
-                                          <?php the_post_thumbnail('post-big-square-thumbnail', array("class" => "featured-image")); ?>
+                                          <?php the_post_thumbnail('post-square-thumbnail', array("class" => "featured-image")); ?>
                                         </div>
                                       </div>
                                     <?php endif; ?>
