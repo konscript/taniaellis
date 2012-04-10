f0b75245d9e8d4512be38e0379f6cdcfd53d213b
Tomas Lieberkind <t.lieberkind@gmail.com>
2012-04-10 13:35:56 +0200

    Fixed minor flaws on about and cases pages

--- wp-content/themes/taniaellis/about.php
+++ wp-content/themes/taniaellis/about.php
@@ -59,7 +59,6 @@
 			  <li><a target="_blank" href="http://www.linkedin.com/groups?mostPopular=&gid=3776885"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_linkedin.png" /></a></li>
 			  <li><a target="_blank" href="http://www.youtube.com/TheNewPioneers"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_youtube.png" /></a></li>
 			  <li><a target="_blank" href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_rss.png" /></a></li>
-			  <li><a target="_blank" href="http://thefairpages.com/company-profile/profile.aspx?company=tania-ellis"><img src="<?php bloginfo('template_url') ?>/images/social_media_icon_fairpages.png" /></a></li>
 			</ul>
 			<div class="box">
         <p class="box-header">
@@ -99,7 +98,7 @@
         <div class="widget widget-key-services">
           <div class="header-container">
             <h2 class="first-line">4 Key Services</h2>
-            <h2 class="second-line">All Centered Around Social Business</h2>
+            <h2 class="second-line">Social and Economic Value Creation</h2>
           </div>
 
 
